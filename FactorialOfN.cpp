6dd20d7a7dc4427b8ac7fff0a1f3f1faffea2056
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-23 23:34:43 +0530

    Create FactorialOfN.cpp

---
+++ FactorialOfN.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long long fact = 1;
+
+    cout << "Enter a Number: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    cout << "Factorial Of " << n << " is: " << fact;
+
+    return 0;
+}
