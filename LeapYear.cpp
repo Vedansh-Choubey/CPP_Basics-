f096cd5788fabe7df6cd968ef698ee4dabc65022
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-24 22:55:35 +0530

    Updating program and commenting out the old method in LeapYear.cpp

--- LeapYear.cpp
+++ LeapYear.cpp
@@ -6,18 +6,25 @@ int main() {
     cout << "Enter Any Year: ";
     cin >> year;
 
-    if (year % 400 == 0) {
-        cout << year << " is a Leap Year";
-    } 
-    else if (year % 100 == 0) {
-        cout << year << " is not a Leap Year";
-    } 
-    else if (year % 4 == 0) {
-        cout << year << " is a Leap Year";
-    } 
-    else {
-        cout << year << " is not a Leap Year";
-    }
+//    if (year % 400 == 0) {                   // One By One Conditions  
+//        cout << year << " is a Leap Year";
+//    } 
+//    else if (year % 100 == 0) {
+//        cout << year << " is not a Leap Year";
+//    } 
+//    else if (year % 4 == 0) {
+//        cout << year << " is a Leap Year";
+//    } 
+//    else {
+//        cout << year << " is not a Leap Year";
+//    }
 
-    return 0;
+//    return 0;
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){   // All Conditions at once 
+        cout<< year<<" is a Leap Year !!";
+        
+    }else {
+        cout<< year<<" is Not a Leap Year !!";
+    }
 }
