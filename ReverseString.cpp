0b678fe9cd291982a04d7ce40e001280bdf484ae
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-24 23:10:22 +0530

    Create ReverseString.cpp

---
+++ ReverseString.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;  
+    string reverse = "";
+    cout <<"Enter Any String: ";
+    cin>>str;
+    for (int i = str.length() - 1; i >= 0; i--) {
+        reverse += str[i];
+    }
+
+    cout << "Reversed: " << reverse;
+
+    return 0;
+}
