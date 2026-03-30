585e59b6a7728c906a22c3b454d3610dd9743cb9
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-30 18:06:38 +0530

    Refactor number swapping to use user input and arithmetic
    Modified the swapping logic to use arithmetic instead of a temporary variable and added user input for values of a and b.

--- NumberSwapping.cpp
+++ NumberSwapping.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 
 int main() {
-    int a = 10, b = 20, temp;
+    int a, b;
+  //  int temp;
+    cout << "Enter Value of a: ";
+    cin>>a;
+    cout << "Enter Value of b: ";
+    cin>>b;    
+//    temp = a;              //using Temp Variable 
+//    a = b;
+//    b = temp;
 
-    temp = a;
-    a = b;
-    b = temp;
-
+    a = a + b;
+    b = a - b;
+    a = a - b
+    
     cout << "After Swapping Values:"<<endl;
-    cout << "a = " << a <<endl;
-    cout <<" b = " << b <<endl;
+    cout <<"a = " << a <<endl;
+    cout <<"b = " << b <<endl;
 
     return 0;
 }
