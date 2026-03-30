fa45190d2540cf50d5ca5f1dc525294bd7be0203
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-30 18:25:58 +0530

    Add area and perimeter calculations for rectangle

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 int main (){
-  double a, b;
+  double a, b, ar, peri;
   cout<<"Enter Side 'A' of Ractangle: ";
   cin>>a;
   cout<<"Enter Side 'B' of Ractangle: ";
   cin>>b;
-
-  cout<<"Area of the Ractangle with sides "<<a<<" and "<<b<<" is: "<<a*b;
+  ar= a*b;
+  peri= 2* (a+b);
+  cout<<"Area of the Ractangle with sides "<<a<<" and "<<b<<" is: "<<ar<<endl;
+  cout<<"and Perimeter of the Ractangle is: "<<peri;
 }
+
