0b1d57cabd9da0fc80f48f0a1cb10e16b2b9cb34
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-30 18:13:02 +0530

    Create Rectangle.cpp to calculate rectangle area
    Fix typos in output prompts and include directive.

---
+++ Rectangle.cpp
@@ -0,0 +1,11 @@
+#inclue <iostream>
+using namespace std;
+int main (){
+  double a, b;
+  cout<<"Enter Side 'A' of Ractangle: ";
+  cin>>a;
+  cout<<"Enter Side 'B' of Ractangle: ";
+  cin>>b;
+
+  cout<<"Area of the Ractangle = "<<a*b;
+}
