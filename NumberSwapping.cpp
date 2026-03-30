76e2fe8bc4ae1f78e44c8922f1079c162ad3e5c3
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-30 18:01:27 +0530

    Improve output formatting after swapping values

---
+++ NumberSwapping.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 20, temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After Swapping Values:"<<endl;
+    cout << "a = " << a <<endl;
+    cout <<" b = " << b <<endl;
+
+    return 0;
+}
