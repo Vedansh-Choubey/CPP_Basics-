c71fe93c11bf6d8f6ab0f1077b80f71b2bf45136
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-03-31 21:29:02 +0530

    Refactor LinkedList to manually create and display nodes
    Updated the LinkedList implementation to manually create nodes and display their values.

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -7,37 +7,61 @@ struct Node {
 };
 
 int main() {
-    int num, value;
-    cout << "Enter number of nodes: ";
-    cin >> num;
+//    int num, value;
+//    cout << "Enter number of nodes: ";
+//    cin >> num;
 
-    Node* head = NULL;
-    Node* temp = NULL;
+//    Node* head = NULL;
+//    Node* temp = NULL;
 
-    for (int i = 0; i < num; i++) {
-        cout << "Enter value: ";
-        cin >> value;
+//    for (int i = 0; i < num; i++) {
+//        cout << "Enter value: ";
+//        cin >> value;
 
-        Node* newNode = new Node();
-        newNode->data = value;
-        newNode->next = NULL;
+//        Node* newNode = new Node();
+//        newNode->data = value;
+//        newNode->next = NULL;
 
-        if (head == NULL) {
-            head = newNode;
-            temp = newNode;
-        } else {
-            temp->next = newNode;
-            temp = newNode;
-        }
-    }
+//        if (head == NULL) {
+//            head = newNode;
+//            temp = newNode;
+//        } else {
+//            temp->next = newNode;
+//            temp = newNode;
+//        }
+//    }
 
-                          // to Display
-    temp = head;
+//                          // to Display
+//    temp = head;
+//    while (temp != NULL) {
+//        cout << temp->data << " -> ";
+//        temp = temp->next;
+//    }
+//    cout << "NULL";
+
+ //   return 0;
+//}
+
+                     // Manually Creating 3 nodes
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+                     // Data Assign krennge 
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = NULL;
+
+                     // List Print krne ke liye 
+    Node* temp = head;
     while (temp != NULL) {
         cout << temp->data << " -> ";
         temp = temp->next;
     }
-    cout << "NULL";
-
     return 0;
 }
