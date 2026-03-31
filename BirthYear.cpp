857c2fd6767701ec2c7eeec46a0a98fdb0c22e0e
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-03-31 21:46:14 +0530

    Enhance birth year calculation with birthday check
    Added logic to account for whether the user has had their birthday this year when calculating birth year.

--- BirthYear.cpp
+++ BirthYear.cpp
@@ -4,12 +4,32 @@ using namespace std;
 int main() {
     int age, birthYear;
 
+//    cout << "Enter your age: ";
+//    cin >> age;
+
+//    birthYear = 2026 - age;      //inaccurate If Bday had not happend in this year yet
+
+//    cout << "Your birth year is: " << birthYear << endl;
+    
+//    return 0;
+//}
+
+
+    char hadBirthday;
+
     cout << "Enter your age: ";
     cin >> age;
 
-    birthYear = 2026 - age;
+    cout << "Have you had your birthday this year? (Y/ N): ";
+    cin >> hadBirthday;
+
+    if (hadBirthday == 'y' || hadBirthday == 'Y'|| hadBirthday == 'Yes'|| hadBirthday == 'YES') {
+        birthYear = 2026 - age;
+    } else {
+        birthYear = 2026 - age - 1;
+    }
 
     cout << "Your birth year is: " << birthYear << endl;
-    
+
     return 0;
 }
