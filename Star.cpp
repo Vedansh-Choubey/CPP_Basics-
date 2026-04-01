946f7bb1f6018b27377a32bb81f48180397c7e7a
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-01 21:52:28 +0530

    Correct inverted right triangle printing logic
    Fix the printing of the inverted right triangle and correct the output statement.

--- Star.cpp
+++ Star.cpp
@@ -22,5 +22,15 @@ int main() {
          }
        cout<<endl;
 }
+    cout <<endl;
+    cout <<"Printing Inverted Right Tringle "<<endl;
+    cout <<endl;
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n -i ; j++){
+            cout <<"* ";
+        }
+        cout <<enl;
+    }
 return 0;
 }
