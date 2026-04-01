8731eea94c0cd290c2920c2b2b135206a368efdb
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-01 22:09:27 +0530

    Implement star pyramid printing in Star.cpp
    Added functionality to print a pyramid pattern with stars.

--- Star.cpp
+++ Star.cpp
@@ -30,7 +30,26 @@ int main() {
         for (int j = 1; j <= n -i +1 ; j++){     //n - i starts with 5 - 1 = 4* !! we want starting with 5*
             cout <<"* ";
         }
-        cout <<enl;
+        cout <<endl;
+    }
+
+    cout <<endl;
+    cout <<"Printing Piramid "<<endl;
+    cout <<endl;
+
+    for(int i = 1; i <= n; i++) {
+
+        // space lane ke liye 
+        for(int j = 1; j <= n - i; j++) {
+            cout << ".";
+        }
+
+        // stars
+        for(int j = 1; j <= 2*i ; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
     }
 return 0;
 }
