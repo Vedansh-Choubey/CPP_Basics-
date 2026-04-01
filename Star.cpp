ddc24351ce2e9540441e7100bf344aaea5fe1503
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-01 21:37:35 +0530

    Enhance Star.cpp to print right triangle pattern
    Added functionality to print a right triangle pattern of asterisks.

--- Star.cpp
+++ Star.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int n = 5;
-
+    cout <<"Printing Simple n×n Square "<<endl;
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
             cout << "* ";
@@ -11,5 +11,16 @@ int main() {
         cout << endl;
     }
 
-    return 0;
+//    return 0;
+ cout << endl;
+ cout <<"Printing Right Triangle "<<endl;
+ cout << endl;
+
+    for (int i = 1; i <=n;  i++){
+         for (int j = 1; j <= i; j++){
+             cout <<"* ";
+         }
+       cout<<endl;
+}
+return 0;
 }
