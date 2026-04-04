b206c89a6acc99077762b8c11de408957c6e2066
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-04 21:57:25 +0530

    Implement ascending and descending order printing
    Added functionality to print the array in ascending and descending order.

--- Sorting.cpp
+++ Sorting.cpp
@@ -8,7 +8,13 @@ int main() {
 
     sort(arr, arr + n);   
 
+    for(int i = 0; i < n; i++) {      //For Ascending Order Printing 
+        cout << arr[i] << " ";
+        }
+    sort(arr, arr + n, greater<int>());   // For printing in Descending order
+
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
+}
 }
