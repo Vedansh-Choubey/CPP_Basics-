2976eea3dc6409815cb63f6cd2f353a5df1e72d1
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-04 22:06:20 +0530

    Add Program in Bubble.Sort

---
+++ BubbleSort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {9, 17, 7, 15, 13};
+    int n = 5;
+
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
