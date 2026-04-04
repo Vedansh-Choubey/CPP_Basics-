3dbe6d38e21ad8a94f84dfba95c99150ad31fe7f
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-04 21:59:36 +0530

    Update Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -6,11 +6,12 @@ int main() {
     int arr[] = {7, 2, 11, 9, 5};
     int n = 5;
 
-    sort(arr, arr + n);   
+//    sort(arr, arr + n);                  //For Ascending Order Printing
 
-    for(int i = 0; i < n; i++) {      //For Ascending Order Printing 
-        cout << arr[i] << " ";
-        }
+//    for(int i = 0; i < n; i++) {       
+//        cout << arr[i] << " ";
+//        }
+
     sort(arr, arr + n, greater<int>());   // For printing in Descending order
 
     for(int i = 0; i < n; i++) {
