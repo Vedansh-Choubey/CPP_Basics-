cf02a2df12e5f299a6f959af5b8f6ee8f448e3df
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-06 22:20:46 +0530

    Add PrimeInRange.cpp - Program to find prime numbers in a given range

---
+++ PrimeInRange.cpp
@@ -0,0 +1,26 @@
+// Program to find prime numbers in a given range
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int start, end;
+    cout << "Enter the start and end of the range: ";
+    cin >> start >> end;
+
+    cout << "Prime numbers in the range are: ";
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
