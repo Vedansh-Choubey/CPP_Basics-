30f3bbe1d9b5828cf57e7de7a92d48b645fe19aa
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-07 22:41:53 +0530

    Calculate hypotenuse instead of area in Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    float Base, Height, area;
+//  float area;
+    float Base, Height, Hypotenuse;
+  
 
-    cout << "Enter base of the triangle: ";
+    cout << "Enter base of the triangle: ";       
     cin >> Base;
 
     cout << "Enter height of the triangle: ";
     cin >> Height;
 
-    area = (Base * Height) / 2;
+//    area = (Base * Height) / 2;
 
-    cout << "Area of triangle = " << area;
+//    cout << "Area of triangle = " << area;
+//    return 0;
+    Hypotenuse = sqrt(Base*Base + Height*Height);      //if Base and Height are Given!!
 
+    cout<<"Hypotenuse of the Tringle will be: "<<Hypotenuse<<endl;
     return 0;
 }
+
+
+    
