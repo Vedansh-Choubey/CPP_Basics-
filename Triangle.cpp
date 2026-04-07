03b11ddf59014c982a4df42cc197341fbe4ae0f4
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-07 22:52:06 +0530

    Refactor triangle calculations and input prompts

--- Triangle.cpp
+++ Triangle.cpp
@@ -7,21 +7,33 @@ int main() {
     float Base, Height, Hypotenuse;
   
 
-    cout << "Enter base of the triangle: ";       
-    cin >> Base;
+//    cout << "Enter base of the triangle: ";       
+//    cin >> Base;
 
-    cout << "Enter height of the triangle: ";
-    cin >> Height;
+//    cout << "Enter height of the triangle: ";
+//    cin >> Height;
 
 //    area = (Base * Height) / 2;
 
 //    cout << "Area of triangle = " << area;
 //    return 0;
-    Hypotenuse = sqrt(Base*Base + Height*Height);      //if Base and Height are Given!!
+//    Hypotenuse = sqrt(Base*Base + Height*Height);      //if Base and Height are Given!!
 
-    cout<<"Hypotenuse of the Tringle will be: "<<Hypotenuse<<endl;
+//    cout<<"Hypotenuse of the Tringle will be: "<<Hypotenuse<<endl;
+//    return 0;
+
+
+      cout << "Enter side of the triangle: ";       
+      cin >> Base;
+
+      cout << "Enter Hypotenuse of the triangle: ";
+      cin >> Hypotenuse;
+
+     Height = (Hypotenuse*Hypotenuse - Base*Base);      //If one side and Hypotenuse is given
+
+    cout<<"The Other Side of The Tringle Will Be: "<<Height<<endl;
     return 0;
-}
+} 
 
 
     
