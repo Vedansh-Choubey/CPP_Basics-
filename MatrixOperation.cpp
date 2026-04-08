56943c766443ff44e63b89438337d5ace9094721
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-08 22:19:18 +0530

    Add matrix addition program for 2x2 matrices
    Implement a program to add two 2x2 matrices and print the result.

---
+++ MatrixOperation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     cout <<"A Program To Print Matrix(a) + Matrix(b)\n";
+     cout <<"This Program Is For 2×2 size Matrix\n";
+  
+    int a[2][2], b[2][2], sum[2][2];
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            cin >> a[i][j];
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            cin >> b[i][j];
+
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<2;j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
