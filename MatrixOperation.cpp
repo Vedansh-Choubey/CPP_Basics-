888acfe506647fcb21f3e9a82fb58d05622b6b0b
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-08 22:50:56 +0530

    Modify matrix operations to include multiplication
    Updated the program to print the product of two matrices instead of their subtraction.

--- MatrixOperation.cpp
+++ MatrixOperation.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() { 
 //     cout <<"A Program To Print Matrix(a) + Matrix(b)\n";
-     cout <<" Aprogram To Print Matrix(a) - Matrix(b)\n";
+//     cout <<" Aprogram To Print Matrix(a) - Matrix(b)\n";
      cout <<"This Program Is For 2×2 size Matrix\n";
-  
+     cout <<"Given Values of both Matrix in a11 a12 a21 a22 order\n";
     int a[2][2], b[2][2], sum[2][2];
 
     for(int i=0;i<2;i++) {                    //Taking Input of Matrix (a)
@@ -29,11 +29,21 @@ int main() {
 //    return 0;
      
   
-     int sub [2][2];
+//    int sub [2][2];
+//     for(int i=0;i<2;i++) {
+//        for(int j=0;j<2;j++) {
+//            sub[i][j] = a[i][j] - b[i][j];    //For subtraction 
+//            cout << sub [i][j] << " ";
+//        }
+//        cout << endl;
+
+    
+     cout <<"A Program To Print Matrix(a) * Matrix(b)\n";
+     int mult [2][2];
      for(int i=0;i<2;i++) {
         for(int j=0;j<2;j++) {
-            sub[i][j] = a[i][j] - b[i][j];    //For subtraction 
-            cout << sub [i][j] << " ";
+            mult[i][j] = a[i][j] * b[i][j];    //For Multiplication 
+            cout << mult [i][j] << " ";
         }
         cout << endl;
      }
