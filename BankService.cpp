b133a85f4abbc76e888069c67a8472345bc26d1e
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-15 22:53:07 +0530

    Implement basic bank account functionality

---
+++ BankService.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount {
+public:
+    string name;
+    int accNo;
+    double balance;
+
+    void create() {
+        cout << "Enter Name: ";
+        cin >> name;
+
+        cout << "Enter Account Number: ";
+        cin >> accNo;
+
+        cout << "Enter Balance: ";
+        cin >> balance;
+    }
+
+    void deposit() {
+        double amount;
+        cout << "Enter amount to deposit: ";
+        cin >> amount;
+        balance = amount;
+    }
+
+    void withdraw() {
+        double amount;
+        cout << "Enter amount to withdraw: ";
+        cin >> amount;
+
+        if (amount > balance)
+            cout << "Insufficient Balance\n";
+        else
+            balance -= amount;
+    }
+
+    void display() {
+        cout << "\nName: " << name;
+        cout << "\nAccount No: " << accNo;
+        cout << "\nBalance: " << balance << endl;
+    }
+};
+
+int main() {
+    BankAccount b;
+    b.create();
+
+    int choice;
+
+    do {
+        cout << "\n1.Deposit\n2.Withdraw\n3.Display4.Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                b.deposit();
+                break;
+            case 2:
+                b.withdraw();
+                break;
+            case 3:
+                b.display();
+                break;
+            case 4:
+                cout << "Thank You\n";
+                break;
+            default:
+                cout << "Invalid choice\n";
+        }
+
+    } while (choice != 4);
+
+    return 0;
+}
